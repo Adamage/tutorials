2b0d7d1d709b6d48f6afe6dc5108f2d719c7009f
Adam Wróbel <adam.wrobel@deepsense.ai>
2021-09-22 13:44:12 +0200

    7 Allow to skip output from a code cell as long as the tag is used in a comment

--- tutorials/sst/tests/test_cli_commands.py
+++ tutorials/sst/tests/test_cli_commands.py
@@ -34,6 +34,31 @@ def test_cli_positive(cli_runner_instance):
     remove_files_if_present([test_output_with_extension])
 
 
+def test_cli_positive_markdown_output_removal_by_tags(cli_runner_instance):
+    example_input = abspath(STATIC_FILES / "code_blocks_with_outputs_to_be_removed.py")
+
+    test_output_with_extension = Path(str(test_output) + '.md')
+    remove_files_if_present([test_output_with_extension])
+
+    result = cli_runner_instance.invoke(
+        cli,
+        ['--source', example_input, "--output", test_output, "--type", "markdown", "--execute"]
+    )
+
+    assert result.exit_code == 0
+    assert os.path.exists(test_output_with_extension)
+
+    # In this file  we expected exactly one print statement to work
+    with open(test_output_with_extension) as f:
+        actual_contents = f.read()
+        assert "Hello sunshine1!" in actual_contents
+        assert "Goodbye sunshine2!" not in actual_contents
+        assert "Hello sunshine3!" not in actual_contents
+        assert "Goodbye sunshine4!" not in actual_contents
+
+    remove_files_if_present([test_output_with_extension])
+
+
 def test_cli_missing_filename(cli_runner_instance):
     result = cli_runner_instance.invoke(cli, ["--output", test_output])
     assert not os.path.exists(test_output)
